0943ce70e54a122abe81c9d13f052659a01c047e
Greg Jurman <jurman.greg@gmail.com>
2015-06-26 18:06:14 -0400

    Less suck

--- work/pcode/test.py
+++ work/pcode/test.py
@@ -1,31 +1,18 @@
 import struct
 import mmap
 import shutil
+from collections import namedtuple as _nt
 
-class AttributeDict(dict):
-    __getattr__ = dict.__getitem__
-    __setattr__ = dict.__setitem__
+
+CodeHeader = _nt("CodeHeader", (
+    "magic", "endian", "revision", "id", "start", "arg_count",
+    "var_count", "max_stack", "symbol_table_offset",
+    "line_table_offset", "object_size", "flags", "compile_time",
+    "refs", "name"))
 
 
 class HeaderFlags(object):
-    CPROC=0x0001
-    INTERNAL=0x0002
-    DEBUG=0x0004
-    DEBUGGER=0x0008
-    NOCASESTRING=0x0010
-    FUNC=0x0020
-    VARARGS=0x0040
-    RECURSIVE=0x0080
-    ITYPE=0x0100
-    ALLOWBRK=0x0200
-    TRUSTED=0x0400
-    NETFILES=0x0800
-    CASESENSITIVE=0x1000
-    QMCALL=0x2000
-    CTYPE=0x4000
-    CLASS=0x8000
-
-    _header_flags = {
+    _flags = {
         0x0001:"CPROC",
         0x0002:"INTERNAL",
         0x0004:"DEBUGMODE",
@@ -44,6 +31,15 @@ class HeaderFlags(object):
         0x8000:"CLASS"
         }
 
+    def __init__(self, flags):
+        self.flags = flags
+
+    def __str__(self):
+        return ", ".join([self._flags[k] for k in self._flags if (self.flags & k==k)])
+
+    def __repr__(self):
+        return "<HeaderFlags: %s>" % self
+
 
 class CodeSegment:
     SYMTAB = 1
@@ -56,70 +52,38 @@ class CodeObject(object):
         self.path = path
         self.fi = open(path, "r+b")
 
-    def _header_flags(self, raw_flags=0):
-        return [HeaderFlags._header_flags[k] for k in iter(HeaderFlags._header_flags) if (raw_flags & k==k)]
-
     def _get_header(self):
         self.fi.seek(0)
         obj = mmap.mmap(self.fi.fileno(), 0)
-        magic = struct.unpack("b", obj[0])[0]
-        endian = {100:"LITTLE", 101: "BIG"}.get(magic, "UNKNOWN")
-        rev = struct.unpack("b", obj[1])[0]
-        _id = struct.unpack("I", obj[2:6])[0]
-        start = struct.unpack("I", obj[6:10])[0]
-        arg_count = struct.unpack("H", obj[10:12])[0]
-        var_count = struct.unpack("H", obj[12:14])[0]
-        max_stack = struct.unpack("H", obj[14:16])[0]
-        sym_tab_offset = struct.unpack("I", obj[16:20])[0]
-        line_tab_offset = struct.unpack("I", obj[20:24])[0]
-        obj_size = struct.unpack("I", obj[24:28])[0]
-        flags_raw = struct.unpack("H", obj[28:30])[0]
-        flags = self._header_flags(flags_raw)
-        compile_time = struct.unpack("I", obj[30:34])[0]
-        refs = struct.unpack("H", obj[34:36])[0]
-        name = str(obj[36:165]).rstrip("\0")
-
-        _header = AttributeDict(
-            magic=magic,
-            endian=endian,
-            rev=rev,
-            id=_id,
-            start=start,
-            arg_count=arg_count,
-            var_count=var_count,
-            max_stack=max_stack,
-            sym_tab_offset=sym_tab_offset,
-            line_tab_offset=line_tab_offset,
-            obj_size=obj_size,
-            flags=flags,
-            compile_time=compile_time,
-            refs=refs,
-            name=name,
-        )
+        h_raw = list(struct.unpack("=bbIIHHHIIIHIH129s", obj[0:165]))
+        h_raw.insert(1, {100:"LITTLE", 101: "BIG"}.get(h_raw[0], "UNKNOWN"))
+        h_raw[-1] = h_raw[-1].rstrip("\0")
+        h_raw[-4] = HeaderFlags(h_raw[-4])
         obj.close()
 
+        _header = CodeHeader(**dict(zip(CodeHeader._fields, h_raw)))
         return _header
 
     def _get_segment(self, segment):
-        seg = None
+        _header = self._get_header()
         obj = mmap.mmap(self.fi.fileno(), 0)
         _start = 0
         _end = 0
         if segment == CodeSegment.SYMTAB:
-            _start = self._get_header().sym_tab_offset
-            _end = self._get_header().obj_size
+            _start = _header.symbol_table_offset
+            _end = _header.object_size
             if _start is 0:
                 return None # No Symbol Table
         elif segment == CodeSegment.LINETAB:
-            _start = self._get_header().line_tab_offset
-            _end = self._get_header().sym_tab_offset
+            _start = _header.line_table_offset
+            _end = _header.symbol_table_offset
             if _start is 0:
                 return None # No Line Table
         elif segment == CodeSegment.CODE:
-            _start = self._get_header().start
-            _end = self._get_header().line_tab_offset
+            _start = _header.start
+            _end = _header.line_table_offset
             if _end is 0:
-                _end = self._get_header().obj_size
+                _end = _header().object_size
         else:
             return None # Nothing here
 
@@ -132,25 +96,30 @@ class CodeObject(object):
         if new_path is None:
             new_path = "%s.noxref" % self.path
 
+        _header = self._get_header()
         shutil.copy(self.path, new_path)
         new_fi = open(new_path, "r+b")
         new_obj = mmap.mmap(new_fi.fileno(), 0)
         obj = mmap.mmap(self.fi.fileno(), 0)
-        obj_size = self._get_header().obj_size
-        sym_tab_off = self._get_header().sym_tab_offset
+        obj_size = _header.object_size
+        sym_tab_off = _header.symbol_table_offset
         if sym_tab_off: obj_size = sym_tab_off
-        line_tab_off = self._get_header().line_tab_offset
+        line_tab_off = _header.line_table_offset
         if line_tab_off: obj_size = line_tab_off
-        new_obj[24:28] = struct.pack("I", obj_size)
-        new_obj[16:20] = struct.pack("I", 0)
-        new_obj[20:24] = struct.pack("I", 0)
+        new_obj[16:28] = struct.pack("=III", 0, 0, obj_size)
         new_obj.resize(obj_size)
         new_obj.flush()
 
         new_obj.close()
         obj.close()
 
+        return CodeObject(new_path)
+
+
 if __name__ == "__main__":
     comp = CodeObject("test.comp")
     print comp._get_header()
-    comp.make_no_xref()
+    print
+
+    new_file = comp.make_no_xref()
+    print new_file._get_header()
